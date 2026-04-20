6abb84dfd85f91bfac0b36355e9da58cac4ce009
Teja260906 <tejareddy2609@gmail.com>
2026-04-20 17:34:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ function showMessage() {
     
     alert("🔥 Welcome Teja! Your cloud site is live!");
 }
+<script>
+const elements = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+elements.forEach(el => observer.observe(el));
+</script>
