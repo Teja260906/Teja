68ebf79a24e8309047be2ea8d80088fcb901a552
Teja260906 <tejareddy2609@gmail.com>
2026-04-20 22:23:35 +0530

    Update script.js

--- PORTFOLIO/script.js
+++ PORTFOLIO/script.js
@@ -22,3 +22,25 @@ document.getElementById("contactForm").addEventListener("submit", (e) => {
   e.preventDefault();
   alert("Message sent! (Backend integration needed)");
 });
+document.getElementById("contactForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const inputs = document.querySelectorAll("input, textarea");
+
+  const data = {
+    name: inputs[0].value,
+    email: inputs[1].value,
+    message: inputs[2].value
+  };
+
+  const res = await fetch("http://13.233.197.196:3000/contact", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+
+  const result = await res.json();
+  alert(result.message);
+});
