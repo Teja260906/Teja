7b2d659f10f5820c5b2735af9fe2dd5f3c900510
Teja260906 <tejareddy2609@gmail.com>
2026-04-20 18:57:04 +0530

    Update script.js

--- PORTFOLIO/script.js
+++ PORTFOLIO/script.js
@@ -15,3 +15,26 @@ const observer = new IntersectionObserver(entries => {
 
 elements.forEach(el => observer.observe(el));
 </script>
+// Load projects from backend
+fetch("http://13.233.197.196:3000/projects")
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  });
+
+// Contact form
+function sendMessage() {
+  fetch("http://13.233.197.196:3000/contact", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name: "Teja",
+      email: "test@gmail.com",
+      message: "Hello!"
+    })
+  })
+  .then(res => res.json())
+  .then(data => alert(data.status));
+}
